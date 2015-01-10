5fe0f286c68fd08e491cbc97f11da4fc95257524
David Jensen <david@jensen47.com>
2015-01-10 14:08:36 -0800

    Using ng-include to dynamically change the trigger section.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,11 +41,17 @@ angular.module('starter.controllers', [])
 
 .controller('NewRuleCtrl', function($scope, $ionicActionSheet, Rules) {
 
-	console.log(Rules)
+	console.log(Rules);
+
+	$scope.triggerName = 'choose';
 
 	$scope.close = function() {
 		$scope.modal.hide();
 	}
+
+  $scope.triggerTemplate = function() {
+    return 'templates/triggers/'+$scope.triggerName+'.html';  
+  };
 
 
 	 /* TODO: Add a new rule  just using local storage, not this setter/getter stuff */
@@ -57,31 +63,33 @@ angular.module('starter.controllers', [])
 		Rules.set(object);
 	}
 	
-   // Triggered on a button click, or some other target
-    $scope.showEvents = function() {
+  // Triggered on a button click, or some other target
+  $scope.showTriggers = function() {
+    //TODO: possibly put this in $scope
+    var triggerButtons = [
+        { text: '<i class="icon ion-ios7-location"></i> Car is Near Location', name: 'near-location' },
+        { text: '<i class="icon ion-battery-low"></i> Battery Level', name:'battery-level' },
+        { text: '<i class="icon ion-clock"></i>  Availability', name:'availability'}
+    ];
+    // Show the action sheet
+    var hideSheet = $ionicActionSheet.show({
+      buttons: triggerButtons,
+      cancelText: 'Cancel',
+      cancel: function() {
+           // add cancel code..
+         },
+      buttonClicked: function(index) {
+        $scope.triggerName = triggerButtons[index].name;
+        return true;
+      }
+    });
 
-      // Show the action sheet
-      var hideSheet = $ionicActionSheet.show({
-        buttons: [
-          { text: '<i class="icon ion-ios7-location"></i> Car is Near Location' },
-          { text: '<i class="icon ion-battery-low"></i> Battery Level' },
-          { text: '<i class="icon ion-clock"></i>  Availability' }
-	    ],
-        cancelText: 'Cancel',
-        cancel: function() {
-             // add cancel code..
-           },
-        buttonClicked: function(index) {
-          return true;
-        }
-      });
+    // For example's sake, hide the sheet after two seconds
+    $timeout(function() {
+      hideSheet();
+    }, 2000);
 
-      // For example's sake, hide the sheet after two seconds
-      $timeout(function() {
-        hideSheet();
-      }, 2000);
-
-    };
+  };
   
 })
 
