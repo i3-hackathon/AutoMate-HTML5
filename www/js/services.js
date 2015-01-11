6e64bbd999a286445433bda5c2df75e45ed058e8
Steve Pepple <spepple@spepple-MacBook-Pro.local>
2015-01-11 00:27:34 -0800

    Create default rules.

--- www/js/services.js
+++ www/js/services.js
@@ -130,66 +130,15 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
-
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-})
-
-
-/**
- * A simple example service that returns some data.
- */
 .factory('Rules', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
   // Some fake testing data
-  var rules = [{
-	  id: 0,
-	  name: 'Find Station',
-	  event: 'Battery Level',
-	  action: 'Find Nearest Station'
-  }];
-
+  var rules = [
+		{ id : 0, "trigger" : { "name" : "near-location", "label" : "Car is Near Location", "options" : { "location" :  "Palo Alto" } }, "action": { "name" : "reserve-station", "label" : "Reserve Station" , "reserve" : { "id" : 0, "name" : "BMW Research Mt View", "price" : "Free" , "details" : "LD, J1772 6.60 kw","address":"160 E. El Camino Real, Mountain View, CA 94040","coords":[37.380644,-122.07419] }}},
+		{ id : 1, "trigger": { "name" : "battery-level", "label" :"Battery Level", "options": { "level" : "82", "above" : "is Above" }},"action":{"name":"stop-charging","label": "Stop Charging", "options" : { "when" : "Now" }}}
+  ];		 		
 
   return {
     all: function() {
@@ -199,10 +148,12 @@ angular.module('starter.services', [])
       // Simple index lookup
       return rules[ruleId];
     },
-	set: function(rule) {
-		rules.push(rule);
-		return;
-	}
+		set: function(rule) {
+			console.log("new rule", rule)
+			console.log(JSON.stringify(rule))
+			rules.push(rule);
+			return;
+		}
   }
 })
 
